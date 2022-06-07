d16ba90a99420e47ed21d8ebabd3b46e4ed6d7be
Edmund <eyu1@ualberta.ca>
2022-06-07 17:23:00 -0600

    finish i am poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Edmund Yu on 2022-06-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
