0c508b9b1351b8e6b52a50ec2f53626da743eee4
Edmund <eyu1@ualberta.ca>
2022-06-07 16:25:45 -0600

    project setup

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Edmund Yu on 2022-06-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
